8013fe5886a5bac3adbb50cd3a05da44a0aea9f6
Gab <gabrielscoelho95@gmail.com>
2024-06-10 16:58:08 -0300

    feat: Configuração env

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const {default: mongoose} = require("mongoose");
+const url = process.env.MONGO_URL;
 
 module.exports = () => {
-    mongoose.connect(process.env.MONGO_URL)
+    console.log(url)
+    mongoose.connect(url)
         .then(() => console.log('MongoDB connected'))
         .catch((err) => console.log(err));
 }
