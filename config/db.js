b8e4f81b528e725de12ea63a374ee44f5f98b437
Gab <gabrielscoelho95@gmail.com>
2024-06-10 17:40:01 -0300

    feat: Retirada de console log e arquivos sem uso

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@ const {default: mongoose} = require("mongoose");
 const url = process.env.MONGO_URL;
 
 module.exports = () => {
-    console.log(url)
     mongoose.connect(url)
         .then(() => console.log('MongoDB connected'))
         .catch((err) => console.log(err));
